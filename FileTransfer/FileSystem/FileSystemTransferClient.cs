9f647a83b5f83eed925a146be6dfeb861fa4a394
Mathieu Cartoixa <mathieu.cartoixa@free.fr>
2014-06-23 11:05:34 +0200

    Add missing file.

---
+++ FileTransfer/FileSystem/FileSystemTransferClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Common.FileTransfer.FileSystem
+{
+    public class FileSystemTransferClient:
+        FileTransferClient
+    {
+
+        public FileSystemTransferClient(Uri baseAddress):
+            base(baseAddress)
+        {
+        }
+
+        protected override Task<TransferableFile> DoDownloadAsync(Uri path)
+        {
+            string localPath=path.LocalPath;
+            var fi=new FileInfo(localPath);
+
+            var ret=new TransferableFile(() => fi.OpenRead(), fi.Length);
+            return Task.FromResult(ret);
+        }
+    }
+}
